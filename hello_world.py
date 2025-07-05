6d33783d91f1a9b5a9d4cb0017ba780083b4ab17
Phone Tshan Kyaw <phonetshankyawpism@gmail.com>
2025-07-04 21:17:37 -0700

    Add median function

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,11 @@
-def mean (num):
-    return sum(num) / len(num)
-
-print("Mean function loaded")
+def median(num):
+    sorted_nums = sorted(num)
+    n = len(sorted_nums)
+    mid = n // 2
+    if n % 2 == 0:
+        return (sorted_nums[mid - 1] + sorted_nums[mid]) / 2
+    else:
+        return sorted_nums[mid]
+    
+print("Median function loaded")
 
